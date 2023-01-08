0d216dabba6e0b8e2e0c25df365606e1a4c8c60b
Filipe <filipeandrade000@gmail.com>
2023-01-08 14:00:59 -0300

    Project hr-eureka-server created

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -3,7 +3,9 @@ package com.example.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @EnableFeignClients
 @SpringBootApplication
